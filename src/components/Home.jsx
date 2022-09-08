2e13d424755aecf31ceca2bf9552209aac25248d
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-09-08 15:43:06 -0300

    req 2 completo

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  state = {
+    listaVazia: true,
+  };
+
+  render() {
+    const { listaVazia } = this.state;
+    return (
+      <div>
+        <input type="text" />
+        {
+          listaVazia && (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default Home;
